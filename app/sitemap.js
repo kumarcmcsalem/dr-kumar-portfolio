b21aa0e5b56e7c7742c0febf6e2185e42a208ab7
Dr Kumar J <kumarcmcsalem@gmail.com>
2026-05-27 15:34:10 +0530

    Added App Router sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://dr-kumar-jagannathan.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
